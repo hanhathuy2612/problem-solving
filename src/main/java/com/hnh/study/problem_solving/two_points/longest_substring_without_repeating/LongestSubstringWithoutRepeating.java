b0600b60876962ca5ecf9872ece243dc55adc785
huyhanhat <withuwe021@gmail.com>
2025-09-29 10:17:42 +0700

    feat: optimize "longest substring without repeating"

--- src/main/java/com/hnh/study/problem_solving/two_points/longest_substring_without_repeating/LongestSubstringWithoutRepeating.java
+++ src/main/java/com/hnh/study/problem_solving/two_points/longest_substring_without_repeating/LongestSubstringWithoutRepeating.java
@@ -1,16 +1,19 @@
 package com.hnh.study.problem_solving.two_points.longest_substring_without_repeating;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class LongestSubstringWithoutRepeating {
     public static void main(String[] args) {
-        String str = "abcdefghtiupLoppn,phy";
-        System.out.println("Result: " + longestSubstringWithoutRepeating(str));
+        String str = "khianhquathunglunglabongdemghibanchan";
+        System.out.println("Best Result: " + longestSubstringWithoutRepeating(str));
     }
 
-    static String longestSubstringWithoutRepeating(String str) {
+    static Result longestSubstringWithoutRepeating(String str) {
         int[] track = new int[128];
         Arrays.fill(track, -1);
+        List<Result> listResults = new ArrayList<>();
 
         int left = 0, best = 0;
         int start = 0;
@@ -24,9 +27,37 @@ public class LongestSubstringWithoutRepeating {
             if (len > best) {
                 start = left;
                 best = len;
+                listResults.add(new Result(
+                        str.substring(start, start + best),
+                        best
+                ));
             }
         }
+        for (Result item : listResults) {
+            System.out.println("sub: " + item);
+        }
         System.out.println("left: " + start + ", best: " + best);
-        return str.substring(start, start + best + 1);
+        return new Result(
+                str.substring(start, start + best),
+                best
+        );
+    }
+
+    public static class Result {
+        String substring;
+        int length;
+
+        public Result(String substring, int length) {
+            this.substring = substring;
+            this.length = length;
+        }
+
+        @Override
+        public String toString() {
+            return "Result{" +
+                    "substring='" + substring + '\'' +
+                    ", length=" + length +
+                    '}';
+        }
     }
 }
